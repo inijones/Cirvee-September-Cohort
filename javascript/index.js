56c19a3b53435b61da1d91f682b3261bf1f1ff69
Ini_Jones <74598414+inijones@users.noreply.github.com>
2025-10-07 10:45:35 +0100

    Update with Assignment 8

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+let myNumber = 1;
+
+console.log(myNumber);
+
+// What is Javascript
+// Setting Up the Development Environment
+// Javascript in Browser
+// Separation of Concerns
+// Javascript in Node
+// Variables
+// Constants
+// Primitive Types
+// Dynamic Tping
+// Objects
+// Arrays
+// Functions
+// Types of Functions
+
